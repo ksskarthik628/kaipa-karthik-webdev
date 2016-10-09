53e1e401647cdd97d670736902752295168c3f5a
ksskarthik628 <ksskarthik628@gmail.com>
2016-10-09 19:46:14 -0400

    Page Service done

--- public/assignment/services/page.service.client.js
+++ public/assignment/services/page.service.client.js
@@ -1,3 +1,75 @@
-/**
- * Created by karthik on 10/9/16.
- */
+(function () {
+    angular
+        .module('WebAppMaker')
+        .factory('PageService', PageService);
+
+    function PageService() {
+
+        var pages = [
+            { "_id": "321", "name": "Post 1", "websiteId": "456" },
+            { "_id": "432", "name": "Post 2", "websiteId": "456" },
+            { "_id": "543", "name": "Post 3", "websiteId": "456" }
+        ];
+
+        var api = {
+            createPage: createPage,
+            findPagesByWebsiteId: findPagesByWebsiteId,
+            findPageById: findPageById,
+            updatePage: updatePage,
+            deletePage: deletePage
+        };
+        return api;
+        
+        function createPage(websiteId, page) {
+            var newPage = {
+                "_id": (new Date()).getTime() + "",
+                "name": page.name,
+                "title": page.title,
+                "websiteId": websiteId
+            };
+            pages.push(newPage);
+            return newPage;
+        }
+
+        function findPagesByWebsiteId(websiteId) {
+            var results = [];
+            for (var i in pages) {
+                if (pages[i].websiteId === websiteId) {
+                    results.push(pages[i]);
+                }
+            }
+            return results;
+        }
+
+        function findPageById(pageId) {
+            for (var i in pages) {
+                if (pages[i]._id === pageId) {
+                    return pages[i];
+                }
+            }
+            return null;
+        }
+
+        function updatePage(pageId, page) {
+            for (var i in pages) {
+                if (pages[i]._id === pageId) {
+                    pages[i].name = page.name;
+                    pages[i].title = page.title;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        function deletePage(pageId) {
+            for (var i in pages) {
+                if (pages[i]._id === pageId) {
+                    pages.splice(i, 1);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+})();

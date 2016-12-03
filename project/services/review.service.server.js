b1a1502dc66e1b4ee3feff32f4848e37750b72f6
ksskarthik628 <ksskarthik628@gmail.com>
2016-12-03 14:35:09 -0500

    Review service server

--- project/services/review.service.server.js
+++ project/services/review.service.server.js
@@ -1,3 +1,90 @@
-/**
- * Created by karthik on 12/1/16.
- */
+module.exports = function (app, models) {
+
+    var movieModel = models.movieModel;
+    var reviewModel = models.reviewModel;
+    var bluebird = require('bluebird');
+
+    app.post('/bbb/user/:uid/movie/:mid', addReview);
+    app.get('/bbb/movie/:mid/reviews', findAllReviewsForId);
+    app.get('/bbb/user/:uid/reviews', findAllReviewsForUserId);
+    app.put('/bbb/review/:rid', updateReview);
+    app.delete('/bbb/review/:rid', deleteReview);
+
+    function addReview(req, res) {
+        var userId = req.params['uid'];
+        var mid = req.params['mid'];
+        var review = req.body;
+        reviewModel
+            .addReview(userId, mid, review)
+            .then(function (review) {
+                res.json(review);
+            }, function (err) {
+                res.status(400).send(err);
+            });
+    }
+
+    function findAllReviewsForId(req, res) {
+        var mid = req.params['mid'];
+        reviewModel
+            .findAllReviewsForId(mid)
+            .then(function (reviews) {
+                res.json(reviews);
+            }, function (err) {
+                res.status(400).send(err);
+            });
+    }
+
+    function findAllReviewsForUserId(req, res) {
+        var userId = req.params['uid'];
+        reviewModel
+            .findAllReviewsForUserId(userId)
+            .then(function (reviews) {
+                var promiseArray = [];
+                var result = [];
+                reviews.forEach(function (review, index, array) {
+                    promiseArray
+                        .push(movieModel
+                            .findMovieById(review._movie)
+                            .then(function (movie) {
+                                if (movie) {
+                                    review.movie = movie;
+                                    result.push(review);
+                                }
+                            }, function (err) {
+                                console.log(err);
+                            }));
+                });
+                bluebird
+                    .all(promiseArray)
+                    .then(function () {
+                        res.json(result);
+                    });
+            }, function (err) {
+                res.status(400).send(err);
+            });
+    }
+
+    function updateReview(req, res) {
+        var reviewId = req.params['rid'];
+        var review = req.body;
+        reviewModel
+            .updateReview(reviewId, review)
+            .then(function (stats) {
+                res.sendStatus(200);
+            }, function (err) {
+                res.sendStatus(400);
+            });
+    }
+
+    function deleteReview(req, res) {
+        var reviewId = req.params['rid'];
+        reviewModel
+            .deleteReview(reviewId)
+            .then(function (stats) {
+                res.sendStatus(200);
+            }, function (err) {
+                res.sendStatus(400);
+            });
+    }
+
+};

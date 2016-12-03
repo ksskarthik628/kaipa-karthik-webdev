52e590c004b815e82c3b4db8f9791969cda0c17c
ksskarthik628 <ksskarthik628@gmail.com>
2016-12-03 10:11:01 -0500

    Review model and schema

--- project/model/review/review.model.server.js
+++ project/model/review/review.model.server.js
@@ -1,3 +1,52 @@
-/**
- * Created by karthik on 12/1/16.
- */
+module.exports = function () {
+
+    var mongoose = require('mongoose');
+    mongoose.Promise = require('bluebird');
+    var ReviewSchema = require('./review.schema.server')();
+    var Review = mongoose.model('Review', ReviewSchema);
+
+    var api = {
+        addReview: addReview,
+        findAllReviewsForMovieId: findAllReviewsForMovieId,
+        findAllReviewsForId: findAllReviewsForId,
+        findAllReviewsForUserId: findAllReviewsForUserId,
+        FindReviewById: findReviewById,
+        updateReview: updateReview,
+        deleteReview: deleteReview
+    };
+    return api;
+
+    function addReview(userId, mid, movieId, review) {
+        review._user = userId;
+        review._movie = mid;
+        review.movieId = movieId;
+        return Review.create(review);
+    }
+
+    function findAllReviewsForMovieId(movieId) {
+        return Review.find({movieId: movieId});
+    }
+    
+    function findAllReviewsForId(mid) {
+        return Review.find({_movie: mid});
+    }
+    
+    function findAllReviewsForUserId(userId) {
+        return Review.find({_user: userId});
+    }
+    
+    function findReviewById(reviewId) {
+        return Review.findById(reviewId);
+    }
+    
+    function updateReview(reviewId, review) {
+        delete review._id;
+        review.timestamp = Date.now();
+        return Review.update({_id: reviewId}, {$set: review});
+    }
+
+    function deleteReview(reviewId) {
+        return Review.remove({_id: reviewId});
+    }
+
+};
--- project/model/review/review.schema.server.js
+++ project/model/review/review.schema.server.js
@@ -1,3 +1,15 @@
-/**
- * Created by karthik on 12/1/16.
- */
+module.exports = function () {
+
+    var mongoose = require('mongoose');
+    var ReviewSchema = mongoose.Schema({
+        title: String,
+        description: String,
+        timestamp: {type: Date, default: Date.now()},
+        movieId: Number,
+        _movie: {type: mongoose.Schema.Types.ObjectId, ref: 'Movie'},
+        _user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+        rating: Number
+    }, {collection: 'bbb.review'});
+    return ReviewSchema;
+
+};

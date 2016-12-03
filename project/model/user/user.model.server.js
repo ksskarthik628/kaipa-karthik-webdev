e66faa0300ae31167e558b04e7878d6e1c6c0b01
ksskarthik628 <ksskarthik628@gmail.com>
2016-12-03 11:14:19 -0500

    User model and schema

--- project/model/user/user.model.server.js
+++ project/model/user/user.model.server.js
@@ -1,3 +1,98 @@
-/**
- * Created by karthik on 12/1/16.
- */
+module.exports = function () {
+
+    var mongoose = require('mongoose');
+    mongoose.Promise = require('bluebird');
+    var UserSchema = require('./user.schema.server')();
+    var User = mongoose.model('User', UserSchema);
+
+    var api = {
+        createUser: createUser,
+        findAllUsers: findAllUsers,
+        findUserById: findUserById,
+        findUserByUsername: findUserByUsername,
+        findUserByCredentials: findUserByCredentials,
+        likeMovie: likeMovie,
+        undoLikeMovie: undoLikeMovie,
+        isMovieLiked: isMovieLiked,
+        addFollower: addFollower,
+        addFollowing: addFollowing,
+        removeFollowing: removeFollowing,
+        removeFollower: removeFollower,
+        isAlreadyFollowing: isAlreadyFollowing,
+        findAllFollowingUsers: findAllFollowingUsers,
+        findAllFollowerUsers: findAllFollowerUsers,
+        updateUser: updateUser,
+        deleteUser: deleteUser
+    };
+    return api;
+
+    function createUser(user) {
+        return User.create(user);
+    }
+    
+    function findAllUsers() {
+        return User.find();
+    }
+
+    function findUserById(userId) {
+        return User.findById(userId);
+    }
+
+    function findUserByUsername(username) {
+        return User.findOne({username: username});
+    }
+    
+    function findUserByCredentials(username, password) {
+        return User.findOne({username: username, password: password});
+    }
+    
+    function likeMovie(userId, mid, movieId) {
+        return User.update({_id: userId}, {$addToSet: {movieLikes: movieId, likes: mid}});
+    }
+    
+    function undoLikeMovie(userId, mid, movieId) {
+        return User.update({_id: userId}, {$pullAll: {movieLikes: [movieId], likes: [mid]}});
+    }
+
+    function isMovieLiked(userId, mid, movieId) {
+        return User.findOne({_id: userId}, {$and: [{movieLikes: {$in: [movieId]}}, {likes: {$in: [mid]}}]});
+    }
+
+    function addFollower(userId, followerId) {
+        return User.update({_id: userId}, {$addToSet: {followers: followerId}});
+    }
+
+    function addFollowing(userId, followingId) {
+        return User.update({_id: userId}, {$addToSet: {following: followingId}});
+    }
+
+    function removeFollowing(userId, followingId) {
+        return User.update({_id: userId}, {$pullAll: {following: followingId}});
+    }
+
+    function removeFollower(userId, followerId) {
+        return User.update({_id: userId}, {$pullAll: {followers: followerId}});
+    }
+    
+    function isAlreadyFollowing(userId, followId) {
+        return User.findOne({_id: userId, following: {$in: [followId]}});
+    }
+    
+    function findAllFollowingUsers(userIds) {
+        return User.find({_id: {$in: userIds}});
+    }
+
+    function findAllFollowerUsers(userIds) {
+        return User.find({_id: {$in: userIds}});
+    }
+
+    function updateUser(userId, user) {
+        delete user._id;
+        return User.update({_id: userId}, {$set: user});
+    }
+
+    function deleteUser(userId) {
+        return User.remove({_id: userId});
+    }
+
+};
--- project/model/user/user.schema.server.js
+++ project/model/user/user.schema.server.js
@@ -1,3 +1,22 @@
-/**
- * Created by karthik on 12/1/16.
- */
+module.exports = function () {
+
+    var mongoose = require('mongoose');
+    var UserSchema = mongoose.Schema({
+        username: {type: String, required: true, unique: true},
+        password: String,
+        firstName: String,
+        lastName: String,
+        facebook: {id: String, token: String},
+        email: String,
+        imgUrl: String,
+        phone: String,
+        likes: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}],
+        movieLikes: [String],
+        followers: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+        following: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+        roles: {type: String, enum: ['user', 'admin'], default: 'user'},
+        dateCreated: {type: Date, default: Date.now()}
+    }, {collection: 'bbb.user'});
+    return UserSchema;
+
+};

2f69892c8ab233a925c1b5f7992234c809bad870
ksskarthik628 <ksskarthik628@gmail.com>
2016-12-04 12:06:54 -0500

    review.service.client.js done

--- public/project/services/review.service.client.js
+++ public/project/services/review.service.client.js
@@ -1,3 +1,45 @@
-/**
- * Created by karthik on 12/4/16.
- */
+(function () {
+
+    angular
+        .module('BBBApp')
+        .factory('ReviewService', ReviewService);
+
+    function ReviewService($http) {
+
+        var api = {
+            addReview: addReview,
+            deleteReview: deleteReview,
+            findAllReviewsForId: findAllReviewsForId,
+            findAllReviewsForUserId: findAllReviewsForUserId,
+            updateReview: updateReview
+        };
+        return api;
+        
+        function addReview(userId, movieId, review) {
+            var url = "/bbb/user/" + userId + "/movie/" + movieId;
+            return $http.post(url, review);
+        }
+
+        function deleteReview(reviewId) {
+            var url = "/bbb/review/" + reviewId;
+            return $http.delete(url);
+        }
+        
+        function findAllReviewsForId(movieId) {
+            var url = "/bbb/movie/" + movieId + "/reviews";
+            return $http.get(url);
+        }
+
+        function findAllReviewsForUserId(userId) {
+            var url = "/bbb/user/" + userId + "/reviews";
+            return $http.get(url);
+        }
+        
+        function updateReview(reviewId, review) {
+            var url = "/bbb/review/" + reviewId;
+            return $http.put(url, review);
+        }
+
+    }
+
+})();

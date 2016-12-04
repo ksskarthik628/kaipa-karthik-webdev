75e04251a5de1cd1008f80b712286599194780c5
ksskarthik628 <ksskarthik628@gmail.com>
2016-12-04 10:28:10 -0500

    Beginning client side

---
+++ public/project/js/app.js
@@ -0,0 +1,4 @@
+(function () {
+    angular
+        .module('BBBApp', ['ngRoute', 'ui.router', 'ngRating', 'infinite-scroll', 'angular-loading-bar']);
+})();
---
+++ public/project/js/config.js
@@ -0,0 +1,3 @@
+/**
+ * Created by karthik on 12/4/16.
+ */
